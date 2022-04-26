8870fccaf0c382cffe4658f95fc322ab8f1ec14d
morecode <761170536@qq.com>
2022-04-26 22:26:15 +0800

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express()
+const port=3000
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`)
+})
+
